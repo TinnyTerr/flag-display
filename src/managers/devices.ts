d1d335d9d961043b9b24bed2a28b3ee438a1df0a
tinnyterr <l.mcdonald@dmnet.uk>
2026-02-04 20:47:09 +0000

    for the 42nd time i give up

--- src/managers/devices.ts
+++
@@ -1,7 +0,0 @@
-import type { TopManager } from "./top.js";
-
-export class DeviceManager {
-	constructor(private top: TopManager) {
-        this.top.eventHandler.on("devices:all", (data) => {});
-    }
-}
